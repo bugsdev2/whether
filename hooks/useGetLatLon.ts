9a97ba96b44800da10a14a6b7c2399f9fbc243f6
Aadi <adithyamanikumar@proton.me>
2024-08-12 18:13:32 +0530

    Fine tuned the hook by adding a typescript interface and an initial state

--- hooks/useGetLatLon.ts
+++ hooks/useGetLatLon.ts
@@ -1,8 +1,34 @@
 import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
 
-export const useGetLanLon = (place: string) => {
-    const [data, setData] = useState<AxiosResponse[] | string>('');
+export interface GeoData {
+    admin1: string;
+    admin2?: string;
+    admin3?: string;
+    country: string;
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+    timezone?: string;
+}
+
+let initialState = [
+    {
+        admin1: '',
+        admin2: '',
+        admin3: '',
+        country: '',
+        id: 0,
+        latitude: 0,
+        longitude: 0,
+        name: '',
+        timezone: '',
+    },
+];
+
+export const useGetLanLon = (place: string): [GeoData[], string] => {
+    const [data, setData] = useState<GeoData[]>(initialState);
     const [error, setError] = useState('');
 
     useEffect(() => {
