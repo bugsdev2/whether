170dfbc163ae4c58b45feac2a29e3fe98e20d0ab
Aadi <adithyamanikumar@proton.me>
2024-08-25 23:46:17 +0530

    Trying to make it from scratch this time without using nativewind

---
+++ app/App.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import HomeScreen from './(tabs)/HomeScreen';
+
+const App = () => {
+    return (
+        <View>
+            <HomeScreen></HomeScreen>
+            <Text>App</Text>
+        </View>
+    );
+};
+
+export default App;
