a17cf9944ec6f2eb3685c3681a93421192d24b4f
Aadi <adithyamanikumar@proton.me>
2024-08-28 02:02:45 +0530

    Fixed useContext issues

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,35 @@
 import { View, StyleSheet, Pressable } from 'react-native';
-import React, { useState, createContext, Dispatch, SetStateAction } from 'react';
+import React, { useState, createContext } from 'react';
 import Search from '@/components/search';
 import { Colors } from '@/constants/Colors';
 import MainCard from './(tabs)/mainCard';
 import { Feather } from '@expo/vector-icons';
 import { getData } from '@/helpers/storage';
 
-export const LocationContext = createContext<React.Context<(string | React.Dispatch<React.SetStateAction<string>>)[]> | null>(null);
+export const LocationContext = createContext<{ searchQuery: string; setSearchQuery: React.Dispatch<React.SetStateAction<string>> }>({ searchQuery: '', setSearchQuery: () => {} });
+export const LatLonProvider = createContext<{ latLonData: {}; setLatLonData: React.Dispatch<React.SetStateAction<{}>> }>({ latLonData: {}, setLatLonData: () => {} });
 
 const App = () => {
     const [searchQuery, setSearchQuery] = useState('');
+    const [latLonData, setLatLonData] = useState({});
     const [searchIconDisplay, setSearchIconDisplay] = useState(true);
 
     function handleIconDisplay() {
         setSearchIconDisplay(true);
     }
 
-    const data = getData('location');
-
     return (
-        <LocationContext.Provider value={[searchQuery, setSearchQuery]}>
-            <View style={styles.container}>
-                <Pressable onPress={() => setSearchIconDisplay(false)}>
-                    <Feather name="search" style={[!searchIconDisplay && { display: 'none' }, styles.searchIcon]} />
-                </Pressable>
-                {searchIconDisplay ? null : <Search iconDisplay={() => handleIconDisplay()} />}
-                <MainCard />
-            </View>
-        </LocationContext.Provider>
+        <LatLonProvider.Provider value={{ latLonData, setLatLonData }}>
+            <LocationContext.Provider value={{ searchQuery, setSearchQuery }}>
+                <View style={styles.container}>
+                    <Pressable onPress={() => setSearchIconDisplay(false)}>
+                        <Feather name="search" style={[!searchIconDisplay && { display: 'none' }, styles.searchIcon]} />
+                    </Pressable>
+                    {searchIconDisplay ? null : <Search iconDisplay={() => handleIconDisplay()} />}
+                    <MainCard />
+                </View>
+            </LocationContext.Provider>
+        </LatLonProvider.Provider>
     );
 };
 
--- components/search.tsx
+++ components/search.tsx
@@ -1,21 +1,47 @@
-import { StyleSheet, TextInput, View } from 'react-native';
-import React, { useState, useContext } from 'react';
+import { FlatList, StyleSheet, TextInput, View, Text, Pressable } from 'react-native';
+import React, { useState, useContext, useLayoutEffect } from 'react';
 import { Colors } from '@/constants/Colors';
-import { LocationContext } from '@/app';
+import { LocationContext } from '@/app/index';
+import { LatLonProvider } from '@/app/index';
 import { useGetLanLon } from '@/hooks/useGetLatLon';
 
 const Search = (props: { iconDisplay: () => void }) => {
-    const [searchQuery, setSearchQuery] = useContext(LocationContext);
+    const { latLonData, setLatLonData } = useContext(LatLonProvider);
+    const { searchQuery, setSearchQuery } = useContext(LocationContext);
 
     function handleSearchQuery(query: string) {
-        // console.log(searchQuery);
+        setSearchQuery(query);
     }
+
+    function handleLocationSelection(name: string, lat: number, lon: number) {
+        setLatLonData({ name, lat, lon });
+        props.iconDisplay();
+    }
+
+    const data = useGetLanLon(searchQuery);
 
     return (
         <View>
             <View style={styles.inputContainer}>
-                <TextInput style={styles.input} placeholder="enter the location here" placeholderTextColor={Colors.darkMode.light} onChangeText={handleSearchQuery} />
+                <TextInput style={styles.input} value={searchQuery} placeholder="enter the location here" placeholderTextColor={Colors.darkMode.light} onChangeText={handleSearchQuery} />
             </View>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.searchSuggestions}>
+                            <Pressable onPress={() => handleLocationSelection(item.name, item.latitude, item.longitude)}>
+                                <Text>
+                                    {item.name}
+                                    {item.admin1 && ', ' + item.admin1 + ','} {item.country}
+                                </Text>
+                            </Pressable>
+                        </View>
+                    );
+                }}
+                keyExtractor={(item) => item.id.toString()}
+                ListEmptyComponent={<View style={{ display: 'none' }}></View>}
+            />
         </View>
     );
 };
@@ -34,9 +60,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         flex: 1,
         borderColor: Colors.darkMode.gray,
-        borderRadius: 15,
+        borderRadius: 30,
         paddingHorizontal: 15,
-        paddingVertical: 5,
+        paddingVertical: 2,
         color: Colors.darkMode.light,
     },
 
@@ -49,5 +75,15 @@ const styles = StyleSheet.create({
 
     btnTxt: {
         color: Colors.darkMode.light,
+    },
+
+    listContainer: {
+        backgroundColor: Colors.darkMode.light,
+    },
+
+    searchSuggestions: {
+        backgroundColor: Colors.darkMode.light,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
     },
 });
