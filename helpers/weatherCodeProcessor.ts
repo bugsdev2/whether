6b8851f950a6a2eee0426ee57d59001db89d1d52
Aadi <adithyamanikumar@proton.me>
2024-08-28 23:28:23 +0530

    Added helper function for processing weather code number

---
+++ helpers/weatherCodeProcessor.ts
@@ -0,0 +1,15 @@
+import weatherCodes from '@/constants/weatherCodes';
+
+export function processWeatherCode(code: number, timeOfDay: string) {
+    for (let key in weatherCodes) {
+        if (code == undefined) return;
+
+        if (code.toString() === key) {
+            if (timeOfDay == 'day') {
+                return weatherCodes[key].day;
+            } else {
+                return weatherCodes[key].night;
+            }
+        }
+    }
+}
